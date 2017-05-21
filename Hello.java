da0eac05215778ddb8602091b486226e44324731
Jonathan <jayala4@masonlive.gmu.edu>
2017-05-21 18:18:47 -0400

    Second commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello{
 	public static void main(String []argv){
-		System.out.println("Hello World");
+		System.out.println("Hello World: "+something());
+	}
+	private static String something(){
+        return "I'm feeling great"; 
 	}
 }
